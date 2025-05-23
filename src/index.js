989ac472956b88f48ab4a9a5c46ec81af2386cd4
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-05-23 22:55:31 +0545

    making stars for raiting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./animation/StarRating";
+
+function Test() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <StarRating color="green" onSetCount={setCount} />
+      <p>The user have rated {count} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRaiting={10} color="red" />
+    <StarRating
+      maxRaiting={5}
+      size={40}
+      color="pink"
+      className="abc"
+      messages={["worst", "bad", "average", "good", "best"]}
+      defaultRaiting={4}
+    />
+
+    <Test />
   </React.StrictMode>
 );
